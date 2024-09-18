b14d2670155aeacd1c1d7f0841c4f22a09fa20de
Harendra Kumar Kanojiya <harendraverma21@gmail.com>
2024-09-18 18:22:55 +0530

    actions

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI
-    ? 'https://astro-shadcn-ui-template.vercel.app'
+    ? 'https://harendra21.github.io'
     : 'http://localhost:4321',
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
-    }),
+    })
   ],
+  base: 'https://github.com/harendra21/harendra21.github.ioyour-repo-name/',
 })
