d8840e37fbbff5e1ef1c94cee167764d8bff4244
Harendra Kumar Kanojiya <harendraverma21@gmail.com>
2025-04-28 14:11:17 +0530

    removed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://harendra21.github.io',
-  base: 'https://github.com/harendra21/harendra21.github.io',
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
